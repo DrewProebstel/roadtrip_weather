39fad813b5915577885c7940fd9640b6c34f3a6d
Drew Proebstel <d.proebstel@gmail.com>
2022-08-09 13:30:17 -0700

    road trip request spec

--- spec/requests/road_trip_request_spec.rb
+++ spec/requests/road_trip_request_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'log in a user end point' do
-  it 'returns a user with good credentials' do
-    User.create(email:"drew@gmail.com", password: "test", password_confirmation: "test")
+  it 'returns road trip information' do
+    User.create(email:"drew@gmail.com", password: "test", password_confirmation: "test", api_key: "1234567890")
     headers = {
-              "email": "drew@gmail.com",
-              "password": "test",
+              "origin": "Denver,CO",
+              "destination": "Pueblo,CO",
+              "api_key": "jgn983hy48thw9begh98h4539h4"
               }
 
 
@@ -17,8 +18,12 @@ RSpec.describe 'log in a user end point' do
     created = JSON.parse(response.body, symbolize_names: true)
 
     expect(created[:data][:attributes][:api_key]).to be_a(String)
-    expect(created[:data][:attributes][:email]).to be_a(String)
-    expect(created[:data][:type]).to eq("users")
+    expect(created[:data][:id]).to eq(null)
+    expect(created[:data][:attributes][:start_city]).to eq("Denver,CO")
+    expect(created[:data][:attributes][:end_city]).to eq("Pueblo,CO")
+    expect(created[:data][:attributes][:travel_time]).to be_a(String)
+    expect(created[:data][:attributes][:travel_time][:weather_at_eta][:temperature]).to be_a(Float)
+    expect(created[:data][:attributes][:travel_time][:weather_at_eta][:conditions]).to be_a(String)
     expect(created[:data][:id]).to be_a(Integer)
   end
 end
