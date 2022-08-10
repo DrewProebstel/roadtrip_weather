ccc18ed127fbea35bf521911842c0c88c296a8ba
Drew Proebstel <d.proebstel@gmail.com>
2022-08-09 18:59:50 -0700

    bad api request spec

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     if User.find_by(api_key: params[:api_key])
       drive_time = MapQuestFacade.find_drive_time(params[:origin],params[:destination])
-      binding.pry
       coordinates = MapQuestFacade.find_coordinates(params[:destination])
       weather = ForecastFacade.find_forecast_for_roadtrip(coordinates)
       road_trip = RoadTrip.new(params[:origin], params[:destination], drive_time, weather)
