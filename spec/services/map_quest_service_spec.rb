b88add993ceda22415d5d11c0a1bddc55338d1c8
Drew Proebstel <d.proebstel@gmail.com>
2022-08-10 00:10:36 -0700

    map quest service

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe MapQuestService do
     	expect(data[:results][0][:locations][0][:latLng][:lng]).to be_a(Float)
     end
   end
+  it 'returns data on geocode' do
+    VCR.use_cassette("map_quest_service_drive_time") do
+    	data = MapQuestService.get_drive_time("boston,MA","portland,OR")
+
+      expect(data).to be_a(Hash)
+    	expect(data[:route][:realTime]).to be_a(Integer)
+    end
+  end
 end
