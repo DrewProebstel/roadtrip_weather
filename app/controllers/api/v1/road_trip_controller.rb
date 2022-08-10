9cd6f21a5fce550e47e4a9de1eaadb027c81d382
Drew Proebstel <d.proebstel@gmail.com>
2022-08-09 18:56:05 -0700

    bad api test passing on postman

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    drive_time = MapQuestFacade.find_drive_time(params[:origin],params[:destination])
-    coordinates = MapQuestFacade.find_coordinates(params[:destination])
-    weather = ForecastFacade.find_forecast_for_roadtrip(coordinates)
-    road_trip = RoadTrip.new(params[:origin], params[:destination], drive_time, weather)
-
-
-    render json: RoadTripSerializer.serialize(road_trip), status: 200
-
+    if User.find_by(api_key: params[:api_key])
+      drive_time = MapQuestFacade.find_drive_time(params[:origin],params[:destination])
+      binding.pry
+      coordinates = MapQuestFacade.find_coordinates(params[:destination])
+      weather = ForecastFacade.find_forecast_for_roadtrip(coordinates)
+      road_trip = RoadTrip.new(params[:origin], params[:destination], drive_time, weather)
+      render json: RoadTripSerializer.serialize(road_trip), status: 200
+    else
+      render json: {error: "Bad Api Key"}, status: 401
+    end
   end
 
 end
