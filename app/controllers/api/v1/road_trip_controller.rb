f1ad1977da36cea26bf66bf80868c392fd05d240
Drew Proebstel <d.proebstel@gmail.com>
2022-08-09 19:09:47 -0700

    passing impossible trip on postman

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,10 +2,14 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     if User.find_by(api_key: params[:api_key])
       drive_time = MapQuestFacade.find_drive_time(params[:origin],params[:destination])
-      coordinates = MapQuestFacade.find_coordinates(params[:destination])
-      weather = ForecastFacade.find_forecast_for_roadtrip(coordinates)
-      road_trip = RoadTrip.new(params[:origin], params[:destination], drive_time, weather)
-      render json: RoadTripSerializer.serialize(road_trip), status: 200
+      if drive_time == nil
+        render json: ImpossibleTripSerializer.serialize(params[:origin],params[:destination]), status: 200
+      else
+        coordinates = MapQuestFacade.find_coordinates(params[:destination])
+        weather = ForecastFacade.find_forecast_for_roadtrip(coordinates)
+        road_trip = RoadTrip.new(params[:origin], params[:destination], drive_time, weather)
+        render json: RoadTripSerializer.serialize(road_trip), status: 200
+      end
     else
       render json: {error: "Bad Api Key"}, status: 401
     end
